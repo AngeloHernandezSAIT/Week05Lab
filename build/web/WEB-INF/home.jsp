ce6f447361b13b870c344e9250e219e8e7f571f7
Angelo Hernandez <122906206+AngeloHernandezSAIT@users.noreply.github.com>
2023-02-12 23:52:15 -0700

    Add files via upload

---
+++ build/web/WEB-INF/home.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : home
+    Created on : 12-Feb-2023, 11:25:50 PM
+    Author     : Cyber
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>Home Page</h1>
+        <h2>Hello ${username}</h2> <br>
+        <a href="login?logout">Log Out</a>
+        <p>
+    </body>
+</html>
---
+++ src/java/servlets/LoginServlet.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.AccountService;
+/**
+ *
+ * @author Cyber
+ */
+public class LoginServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        
+        if (request.getParameter("logout")!=null){
+         session.invalidate();
+         String message ="You have succesfully logged out";
+         request.setAttribute("message", message);
+         session = request.getSession();
+        }
+        
+        if(session.getAttribute("username")!=null){
+            response.sendRedirect("home");
+        }
+        else
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
+                .forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        AccountService as = new AccountService();
+        
+        HttpSession session = request.getSession();
+        
+        if(as.login(username, password)!=null){
+            session.setAttribute("username", username);
+            session.setAttribute("password", null);
+            response.sendRedirect("home");
+        }
+        else {
+            String message="Username and/or password are invalid";
+            session.setAttribute("message", message);          
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
+                .forward(request, response);
+        
+        }
+    }
+
+
+}
